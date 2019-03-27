4fb504355962050e2d06b844c0ab109d3af49c8f
James McHugh <james.mchugh1988@gmail.com>
2019-03-27 12:01:01 +0000

    added css

--- lsapp/resources/views/layouts/app.blade.php
+++ lsapp/resources/views/layouts/app.blade.php
@@ -1,11 +1,11 @@
 <!doctype html>
 <html lang="{{ config('app.locale') }}">
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>{{$title}}</title>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link type="text/css" rel="stylesheet" href="{{asset('css/app.css') }}" />
+      <title>{{$title}}</title>
 
 
     </head>
