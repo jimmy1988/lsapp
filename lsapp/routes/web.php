28e25f3c84292607863f68dde9eef4051f1a16dc
James McHugh <james.mchugh1988@gmail.com>
2019-03-27 10:48:13 +0000

    new routes - testing

--- lsapp/routes/web.php
+++ lsapp/routes/web.php
@@ -11,6 +11,31 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/hello-html', function () {
+//     return "<h1>Hello World</h1>";
+// });
+
+// Route::get('/hello', function () {
+//     return "Hello World";
+// });
+
+// Route::get('/services', function () {
+//     return view('pages.services');
+// });
+
+// Route::get('/about', function () {
+//     return view('pages.about');
+// });
+
+Route::get('/users/{id}/{name}', function ($id, $name) {
+    return "This is the user " . $name . " with an id of " . $id;
 });
+
+// Route::get('/welcome', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', "PagesController@index");
+Route::get('/welcome', "PagesController@welcome");
+Route::get('/about', "PagesController@about");
+Route::get('/services', "PagesController@services");
