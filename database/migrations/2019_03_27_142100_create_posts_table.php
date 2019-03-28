584e4459111a01e1e49545cf28b3afe6c94bdc65
James McHugh <james.mchugh1988@gmail.com>
2019-03-28 07:42:09 +0000

    new migrations and config

--- database/migrations/2019_03_27_142100_create_posts_table.php
+++ database/migrations/2019_03_27_142100_create_posts_table.php
@@ -18,7 +18,7 @@ class CreatePostsTable extends Migration
             $table->string('title');
             $table->string('body');
             $table->string('user');
-            $table->timestamps();
+            $table->timestamp();
         });
     }
 
