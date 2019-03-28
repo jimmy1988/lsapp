41adb74b102b8e9a0a71c108a3cdf35e3201482f
James McHugh <james.mchugh1988@gmail.com>
2019-03-28 09:37:21 +0000

    removed users fields

--- database/migrations/2019_03_27_142100_create_posts_table.php
+++ database/migrations/2019_03_27_142100_create_posts_table.php
@@ -16,8 +16,7 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('title');
-            $table->string('body');
-            $table->string('user');
+            $table->mediumText('body');
             $table->timestamps();
         });
     }
